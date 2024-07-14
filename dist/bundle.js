3512093e2ca11c0e698d0817b5f4e2a843e8b07a
seungzaee <seungzaee@naver.com>
2024-07-14 18:36:54 +0900

    button eventlistner

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,7 +2,13 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("Hello js");
+const n = document.querySelector(".name");
+/*변수명을 name으로 하면 오류가 난다.*/
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  n.style.color = "red";
+});
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSx3QiIsInNvdXJjZXMiOlsid2VicGFjazovL2Nsb25lX2NvZGluZy8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvIGpzXCIpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxDQUFDLEUiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jbG9uZV9jb2RpbmcvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgbiA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIubmFtZVwiKTtcclxuLyrrs4DsiJjrqoXsnYQgbmFtZeycvOuhnCDtlZjrqbQg7Jik66WY6rCAIOuCnOuLpC4qL1xyXG5jb25zdCBidG4gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiYnV0dG9uXCIpO1xyXG5cclxuYnRuLmFkZEV2ZW50TGlzdGVuZXIoXCJjbGlja1wiLCBmdW5jdGlvbiAoKSB7XHJcbiAgbi5zdHlsZS5jb2xvciA9IFwicmVkXCI7XHJcbn0pOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello js");
+const n = document.querySelector(".name");
+/*변수명을 name으로 하면 오류가 난다.*/
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  n.style.color = "red";
+});
